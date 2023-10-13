00591ba8ef3525c9c5d217fda1ee859f4558839e
ahmedlldev <ahmedll.shafiey@gmail.com>
2023-10-14 00:07:42 +0300

    Initial

---
+++ Two Sum.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] + nums[i + 1] == target) {
+      console.log(i + 1);
+    }
+  }
+};
+
+twoSum([2, 7, 11, 15], 9);
